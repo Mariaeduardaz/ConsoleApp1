25401ea057b677989692e48cb446331f3fb93f42
Mariaeduardaaz1307 <Mariaeduardaaz1307@gmail.com>
2022-02-13 16:28:56 -0300

    robo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,10 +20,10 @@ namespace ConsoleApp1
                         localizacaoY = localizacaoY + 1;
 
                     else if (sentido == 'S')
-                        localizacaoX = localizacaoX - 1;
+                        localizacaoY = localizacaoY - 1;
 
                     else if (sentido == 'L')
-                        localizacaoY = localizacaoY + 1;
+                        localizacaoX = localizacaoX + 1;
 
                     else if (sentido == 'O')
                          localizacaoX = localizacaoX - 1;
@@ -40,8 +40,8 @@ namespace ConsoleApp1
                     else if (sentido == 'L') sentido = 'N';
                 }
 
-                if (instrucoes[posicao] == 'D')
-                {
+               if (instrucoes[posicao] == 'D')
+               {
                     if (sentido == 'N') sentido = 'L';
                     else if (sentido == 'O') sentido = 'N';
 
@@ -50,7 +50,7 @@ namespace ConsoleApp1
                     else if (sentido == 'L') sentido = 'S';
 
 
-                }
+               }
                    
                 
 
@@ -65,9 +65,70 @@ namespace ConsoleApp1
             Console.WriteLine("Localizacao y " + localizacaoY);
             Console.WriteLine("sentido " + sentido);
             Console.ReadLine();
-              
-            
-            
+
+
+            string comando2 = "MMDMMDMDDM";
+            char sentido2 = 'L';
+            int localizacaoY2 = 3;
+            int localizacaoX2 = 3;
+
+            char[] instrucoes2 = comando2.ToCharArray();
+            for (int posicao2 = 0; posicao2 < instrucoes2.Length; posicao2++)
+            {
+                if (instrucoes2[posicao2] == 'M')
+                {
+                    if (sentido == 'N')
+                        localizacaoY2 = localizacaoY2 + 1;
+
+                    else if (sentido == 'S')
+                        localizacaoY2 = localizacaoY2 - 1;
+
+                    else if (sentido == 'L')
+                        localizacaoX2 = localizacaoX2 + 1;
+
+                    else if (sentido == 'O')
+                        localizacaoX2 = localizacaoX2 - 1;
+
+                }
+
+                if (instrucoes2[posicao2] == 'E')
+                {
+                    if (sentido2 == 'N') sentido2 = 'O';
+                    else if (sentido2 == 'O') sentido2 = 'S';
+
+                    else if (sentido2 == 'S') sentido2 = 'L';
+
+                    else if (sentido2 == 'L') sentido2 = 'N';
+                }
+
+                if (instrucoes2[posicao2] == 'D')
+                {
+                    if (sentido2 == 'N') sentido2 = 'L';
+                    else if (sentido2 == 'O') sentido2 = 'N';
+
+                    else if (sentido2 == 'S') sentido2 = 'O';
+
+                    else if (sentido2 == 'L') sentido2 = 'S';
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+            Console.WriteLine("Localizacao x " + localizacaoX2);
+            Console.WriteLine("Localizacao y " + localizacaoY2);
+            Console.WriteLine("sentido " + sentido2);
+            Console.ReadLine();
+
+
+
 
 
 
